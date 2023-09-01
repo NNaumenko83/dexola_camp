61ced54e24a1c62a620c0e7f8a784633cc631180
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-09-01 23:27:30 +0300

    UPD: features images

--- src/constans/featuresImages.ts
+++ src/constans/featuresImages.ts
@@ -5,19 +5,19 @@ import featuresImageSecondMinMobile2x from "../assets/features-images/FeaturesIm
 import featuresImageThirdMinMobile from "../assets/features-images/FeaturesImages/Mobile//Features-image-third_mobile-min.jpg";
 import featuresImageThirdMinMobile2x from "../assets/features-images/FeaturesImages/Mobile/Features-image-third_mobile@2x-min.jpg";
 
-import featuresImageFirstMinTablet from "../assets/features-images/FeaturesImages/Tablet/Features-image-first_Tablet-min.jpg";
-import featuresImageFirstMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-first_Tablet@2x-min.jpg";
-import featuresImageSecondMinTablet from "../assets/features-images/FeaturesImages/Tablet/Features-image-second_Tablet-min.jpg";
-import featuresImageSecondMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-second_Tablet@2x-min.jpg";
-import featuresImageThirdMinTablet from "../assets/features-images/FeaturesImages/Tablet//Features-image-third_Tablet-min.jpg";
-import featuresImageThirdMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-third_Tablet@2x-min.jpg";
+import featuresImageFirstMinTablet from "../assets/features-images/FeaturesImages/Tablet/Features-image-first_tablet-min.jpg";
+import featuresImageFirstMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-first_tablet@2x-min.jpg";
+import featuresImageSecondMinTablet from "../assets/features-images/FeaturesImages/Tablet/Features-image-second_tablet-min.jpg";
+import featuresImageSecondMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-second_tablet@2x-min.jpg";
+import featuresImageThirdMinTablet from "../assets/features-images/FeaturesImages/Tablet//Features-image-third_tablet-min.jpg";
+import featuresImageThirdMinTablet2x from "../assets/features-images/FeaturesImages/Tablet/Features-image-third_tablet@2x-min.jpg";
 
-import featuresImageFirstMinDesktop from "../assets/features-images/FeaturesImages/Desktop/Features-image-first_Desktop-min.jpg";
-import featuresImageFirstMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-first_Desktop@2x-min.jpg";
-import featuresImageSecondMinDesktop from "../assets/features-images/FeaturesImages/Desktop/Features-image-second_Desktop-min.jpg";
-import featuresImageSecondMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-second_Desktop@2x-min.jpg";
-import featuresImageThirdMinDesktop from "../assets/features-images/FeaturesImages/Desktop//Features-image-third_Desktop-min.jpg";
-import featuresImageThirdMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-third_Desktop@2x-min.jpg";
+import featuresImageFirstMinDesktop from "../assets/features-images/FeaturesImages/Desktop/Features-image-first_desktop-min.jpg";
+import featuresImageFirstMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-first_desktop@2x-min.jpg";
+import featuresImageSecondMinDesktop from "../assets/features-images/FeaturesImages/Desktop/Features-image-second_desktop-min.jpg";
+import featuresImageSecondMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-second_desktop@2x-min.jpg";
+import featuresImageThirdMinDesktop from "../assets/features-images/FeaturesImages/Desktop//Features-image-third_desktop-min.jpg";
+import featuresImageThirdMinDesktop2x from "../assets/features-images/FeaturesImages/Desktop/Features-image-third_desktop@2x-min.jpg";
 
 export const featuresImagesArrayMobile = [
 	[featuresImageFirstMinMobile, featuresImageFirstMinMobile2x],
